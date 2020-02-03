f7865ef9239ad388e48151a0aa5c75a13f15f1cc
ashaban@sms1.local <ashaban@agtinternational.com>
2020-02-03 13:26:35 +0100

    This commit optimizes run-all.sh file
    Although it comes after the test solution was submitted, this commit
    aims at optimizing the bash script that runs all the solution by moving
    the SQL commands into separate files. The result and behavior does not change
    However, this commit makes the file run-all.sh more readable

--- bin/run-all.sh
+++ bin/run-all.sh
@@ -4,17 +4,7 @@ echo "Starting cluster"
 MASTER_EXTERNAL_PORT=5422 COMPOSE_PROJECT_NAME=citus docker-compose up --scale worker=3 -d
 
 echo "Start psql shell on the master node, create and distribute the database"
-docker exec citus_master psql -U postgres -d postgres -c "CREATE TABLE installs_by_country(\
-id INT NOT NULL PRIMARY KEY, \
-index INT, \
-country VARCHAR(2), \
-created_at VARCHAR, \
-paid BOOLEAN, \
-installs INT);"
-docker exec citus_master psql -U postgres -d postgres -c "SET citus.replication_model = 'streaming';"
-docker exec citus_master psql -U postgres -d postgres -c "SELECT create_distributed_table('installs_by_country', 'id');"
+docker exec citus_master sh -c "psql -U postgres < /data/setup_tables.sql"
 
 echo "Insert data into the servers"
-docker exec citus_master psql -U postgres -d postgres -c "\copy installs_by_country from 'data/shard1.csv' with csv HEADER;"
-docker exec citus_master psql -U postgres -d postgres -c "\copy installs_by_country from 'data/shard2.csv' with csv HEADER;"
-docker exec citus_master psql -U postgres -d postgres -c "\copy installs_by_country from 'data/shard3.csv' with csv HEADER;"
+docker exec citus_master sh -c "psql -U postgres < /data/import_data.sql"
--- data/import_data.sql
+++ data/import_data.sql
@@ -1,3 +1,3 @@
-\copy installs_by_country from 'shard1.csv' with csv HEADER
-\copy installs_by_country from 'shard2.csv' with csv HEADER
-\copy installs_by_country from 'shard3.csv' with csv HEADER
+\copy installs_by_country from 'data/shard1.csv' with csv HEADER
+\copy installs_by_country from 'data/shard2.csv' with csv HEADER
+\copy installs_by_country from 'data/shard3.csv' with csv HEADER
